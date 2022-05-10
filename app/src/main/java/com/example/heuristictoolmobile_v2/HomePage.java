075d1a760447e43b8cd253bac6c6b5b19efa5454
SrPio <andresfer1799@gmail.com>
2022-05-10 00:39:11 -0500

    Creacion del modulo CrearEvaluacion.java

--- app/src/main/java/com/example/heuristictoolmobile_v2/HomePage.java
+++ app/src/main/java/com/example/heuristictoolmobile_v2/HomePage.java
@@ -2,13 +2,29 @@ package com.example.heuristictoolmobile_v2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class HomePage extends AppCompatActivity {
+
+    ImageView btn_crearEV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
+
+        btn_crearEV = findViewById(R.id.imageViewCrearEv);
+
+        btn_crearEV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), CrearEvaluacion.class);
+                startActivity(intent);
+            }
+        });
     }
 }
